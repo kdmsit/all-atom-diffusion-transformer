ea9ede591e80fe8d7cbb4b49a53339cf7a23cc8d
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-04-10 19:36:25 +0530

    push from local

--- src/train_autoencoder.py
+++ src/train_autoencoder.py
@@ -58,36 +58,27 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
         L.seed_everything(cfg.seed, workers=True)
 
     log.info(f"Instantiating datamodule <{cfg.data.datamodule._target_}>")
-    datamodule: LightningDataModule = hydra.utils.instantiate(
-        cfg.data.datamodule, _recursive_=False
-    )
+    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data.datamodule, _recursive_=False)
 
     log.info(f"Instantiating autoencoder model <{cfg.autoencoder_module._target_}>")
     model: LightningModule = hydra.utils.instantiate(cfg.autoencoder_module)
 
-    log.info("Instantiating loggers...")
-    logger: List[Logger] = instantiate_loggers(cfg.get("logger"))
+    # log.info("Instantiating loggers...")
+    # logger: List[Logger] = instantiate_loggers(cfg.get("logger"))
 
     log.info("Instantiating callbacks...")
-    callbacks: List[Callback] = instantiate_callbacks(
-        cfg.get("callbacks"), using_logger=(len(logger) > 0)
-    )
+    callbacks: List[Callback] = instantiate_callbacks(cfg.get("callbacks"))
+    # callbacks: List[Callback] = instantiate_callbacks(cfg.get("callbacks"), using_logger=(len(logger) > 0))
 
     log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
     trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
 
-    object_dict = {
-        "cfg": cfg,
-        "datamodule": datamodule,
-        "model": model,
-        "callbacks": callbacks,
-        "logger": logger,
-        "trainer": trainer,
-    }
+    # object_dict = {"cfg": cfg, "datamodule": datamodule, "model": model, "callbacks": callbacks, "logger": logger, "trainer": trainer,}
+    object_dict = {"cfg": cfg, "datamodule": datamodule, "model": model, "callbacks": callbacks, "trainer": trainer, }
 
-    if logger:
-        log.info("Logging hyperparameters!")
-        log_hyperparameters(object_dict)
+    # if logger:
+    #     log.info("Logging hyperparameters!")
+    #     log_hyperparameters(object_dict)
 
     if cfg.get("train"):
         log.info("Starting training!")
@@ -127,9 +118,7 @@ def main(cfg: DictConfig) -> Optional[float]:
     metric_dict, _ = train(cfg)
 
     # safely retrieve metric value for hydra-based hyperparameter optimization
-    metric_value = get_metric_value(
-        metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
-    )
+    metric_value = get_metric_value(metric_dict=metric_dict, metric_name=cfg.get("optimized_metric"))
 
     # return optimized metric
     return metric_value
